be6f1455c6f8ba3331b71435c772dfe33e8e5ae8
Yi-Wei Chang <jchang6513@gmail.com>
2019-09-01 17:03:15 +0800

    render with layout file

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,36 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import '../assets/scss/keyboard.scss';
 
-import "../assets/scss/keyboard.scss";
+import ansi104 from '../assets/layout/ANSI104.json';
+
+type TKey = string | {[key: string]: number};
+type KeyRow = TKey[];
+type KeyMap = KeyRow[];
 
 const Keyboard = () => {
+  const keyMap: KeyMap = ansi104;
+  const unit = '60px';
+  const unitWidth = (value: number) => `calc(${value} * ${unit})`;
+
+  let nextKeyWidth = 1;
+  let nextKeyHeight = 1;
+  let maxKeyWidth = 0;
+  let currentRowWidth = 0;
+
+  useEffect(() => {
+    (document.getElementById('keyboard') as any).style.width = `calc(${maxKeyWidth} * ${unit})`
+    console.log(maxKeyWidth);
+  }, [maxKeyWidth]);
+
   return (
-    <div className="keyboard">
-      <div className="row r1-1">
-      <div className="keycap key-125u key-Ctrl">
-          Ctrl
-        </div>
-        <div className="keycap key-125u key-Ctrl">
-          Win
-        </div>
-        <div className="keycap key-125u key-Ctrl">
-          Alt
-        </div>
-        <div className="keycap key-625u key-Ctrl">
-          Space
-        </div>
-        <div className="keycap key-125u key-Ctrl">
-          Alt
-        </div>
-        <div className="keycap key-125u key-Ctrl">
-          Win
-        </div>
-        <div className="keycap key-125u key-Ctrl">
-          ⌥
-        </div>
-        <div className="keycap key-125u key-Ctrl">
-          Ctrl
-        </div>
-      </div>
+    <div id="keyboard">
+      {
+        keyMap.map(row => {
+          if ( maxKeyWidth < currentRowWidth) {
+            maxKeyWidth = currentRowWidth;
+          }
+          nextKeyWidth = 1;
+          currentRowWidth = 0;
+          return (
+            <div className="row" style={{height: unit}}>
+              {
+                row.map(key => {
+                  const keyWidth = nextKeyWidth;
+                  const keyHeight = nextKeyHeight
+                  nextKeyWidth = 1;
+                  nextKeyHeight = 1;
+                  console.log(key, keyWidth, keyHeight)
+                  if (typeof key === 'object') {
+                    if (key.w) {
+                      nextKeyWidth = key.w
+                    }
+                    if (key.h) {
+                      nextKeyHeight = key.h
+                    }
+                    if (key.x) {
+                      currentRowWidth += key.x;
+                      return (
+                        <div className="key-space" style={{height: unit, width: unitWidth(key.x)}} />
+                      )
+                    }
+                  } else {
+                    currentRowWidth += keyWidth;
+                    return (
+                      <div className="keycap" style={{height: unitWidth(keyHeight), width: unitWidth(keyWidth)}}>
+                        {
+                          key.split('\n').map(k => (
+                            <p>
+                              {k}
+                              <br/>
+                            </p>
+                          ))
+                        }
+                      </div>
+                    )
+                  }
+                })
+              }
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
